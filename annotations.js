0d5bcea09125ccfd27d23d0f8a92b36d01e3e6b0
Yuri Sulyma <yuri@lmqm.xyz>
2019-08-22 06:38:04 -0400

    Update email address

--- annotations.js
+++ annotations.js
@@ -6,7 +6,7 @@
  *  
  *  ---------------------------------------------------------------------
  *  
- *  Copyright (c) 2013 Yuri Sulyma <yuri@sulyma.ca>.
+ *  Copyright (c) 2013 Yuri Sulyma <yuri@lmqm.xyz>.
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
